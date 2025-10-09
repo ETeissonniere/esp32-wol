4153c3fb782d5ae10562d89851356de9539e4dae
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2025-10-08 22:04:45 -0700

    fix comment

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -5,8 +5,8 @@
 /**
  * @brief Initialise, attach and start the WiFi interface.
  *
- * The caller must ensure that `esp_netif_init()` and `esp_event_loop_create_default()`
- * have been invoked before calling this function. The implementation assumes there is
- * exactly one Ethernet port configured via `ethernet_init_all`.
+ * The caller must ensure that `esp_netif_init()` and
+ * `esp_event_loop_create_default()` have been invoked before calling this
+ * function.
  */
 esp_err_t wifi_app_start(void);
